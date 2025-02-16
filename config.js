19f5a053bb857a19423fe3df023c51c307be3817
SYKO CODER <121161813+SYKO-CODER@users.noreply.github.com>
2025-02-16 19:51:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
-MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
+SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=qZ0iFCZC#DHnA2O6UAzlJx-UZcsxLcKe4cfB3Iv3A7UlwyWUP9ns', //add your session id
+MONGODB: process.env.MONGODB || "mongodb+srv://Dinuka:#2005Dinux@cluster0.ywgh7.mongodb.net/", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
 };
